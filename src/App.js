d62d8528814d6c210d0a9916dd46e365953d8024
unknown <pedro.portacio@sempreceub.com>
2019-10-07 21:12:26 -0300

    Primeiro projeto teste

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Jaja ta pronta<code> essa merda </code> vou subir pro git.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.globo.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
